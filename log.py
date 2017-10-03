6203670aaf185879f8f8512c08da12c87af45f70
Krisztian Schaffer <schaffer.krisztian@gmail.com>
2017-10-03 15:46:20 +0200

    Logging basics

---
+++ log.py
@@ -0,0 +1,13 @@
+import sys, logging
+
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+root.addHandler(ch)
+
+def getLogger(name):
+    return logging.getLogger(name)
