43107690a45e715f33fde09246fc31b7f3e32ebb
Schaffer Krisztian <sk@ecostudio.hu>
2017-10-11 09:14:57 +0000

    live version of uci.sh

--- uci.sh
+++ uci.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-source activate tf13
-cd /home/krisztian/projects/humanlike/turk && python -u uci.py --logdir=/mnt/red/train/humanlike/old/1/logdir/ 2>&1 | tee -a log.txt
+cd /home/k.o/humanlike/turk && python -u uci.py --logdir=../logdir 2>&1 | tee -a log.txt
